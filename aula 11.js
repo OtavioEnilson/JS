5f2ffeb3f0a4a624f73d7535cdb1ff8d3f89bc9b
OtavioEnilson <otavio-tavinho02@hotmail.com>
2023-05-23 23:57:24 -0300

    Update aula 11.js

--- aula 11.js
+++ aula 11.js
@@ -1,5 +1,5 @@
-var vel= 60.5
-console.log(`A velocidade do seu carro é ${vel}Km/h`
+var vel= 60
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
 
 if (vel > 60) {
     console.log(`VOCE FOI MULTADO!`)
