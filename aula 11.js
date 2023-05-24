9fefe656aca403dca2f31ca5ec09e25f488b4380
OtavioEnilson <otavio-tavinho02@hotmail.com>
2023-05-23 23:55:43 -0300

    Create aula 11.js

---
+++ aula 11.js
@@ -0,0 +1,8 @@
+var vel= 60.5
+console.log(`A velocidade do seu carro é ${vel}Km/h`
+
+if (vel > 60) {
+    console.log(`VOCE FOI MULTADO!`)
+} 
+
+console.log(`Dirija sempre com o cinto de segurança`)
