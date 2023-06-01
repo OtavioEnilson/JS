1a7c854e9da91384cd4ceb09b08a9328b7af91e3
OtavioEnilson <otavio-tavinho02@hotmail.com>
2023-06-01 00:06:50 -0300

    1

--- aula 06/script.js
+++ aula 06/script.js
@@ -1,22 +1,28 @@
 
 function carregar () {
     var msg = window.document.getElementById('msg')
-    var img = window.document.getElementById ('imagem')
-    var data = new Date ()
-    var hora = data.getHours ()
-    msg.innerHTML = `Agora são: ${hora} horas.`
+    var img = window.document.getElementById ('img')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são: ${hora} horas.` 
+
+    
 
     if (hora >=0 && hora < 12) {
         //BOM DIA!
-        img.src= 'manhã.png'
+        img.src= 'manhacertoo.png'
+        document.body.style.background = "blue"
+        
     }
         else if(hora >=12 && hora < 18 ) {
         //BOA TARDE! 
-        img.src= 'tarde.png'
+        img.src= 'tardecerto.png'
+        document.body.style.background = "grey"
     }
         else {
         //BOA NOITE!
-        img.src= 'noite.png'
+        img.src= 'noitecerto.png'
+        document.body.style.background = "black"
         }
 
    
